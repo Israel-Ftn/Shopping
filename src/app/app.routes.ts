2d9e007b4014b6435e2be19d1fbf7689d9a34685
Israel Fiorentini <israelfiorentini189@gmail.com>
2025-02-06 11:49:36 -0300

    Criando a barra de  navegação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RecuperandoComponent } from './recuperando/recuperando.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 export const routes: Routes = [
     {path:'login', title:"logando", component:LoginComponent},
-    {path:'RecuperacaoSenha', title:'RecSenha', component:RecuperandoComponent}
+    {path:'RecuperacaoSenha', title:'RecSenha', component:RecuperandoComponent},
+    {path:'PaginaInicial', title:'home', component:HomeComponent},
+    {path:'nav', title:'Navegacao' , component:NavbarComponent} 
 ];
--- src/app/recuperando/recuperando.component.ts
+++ src/app/recuperando/recuperando.component.ts
@@ -8,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './recuperando.component.css'
 })
 export class RecuperandoComponent {
-
+  Resposta:string =''
+envio() {
+  let email = (<HTMLInputElement>document.getElementById('email')).value;
+  if (email == ''){
+   this.Resposta ='Digite um Email válido'
+  }
+  else{
+    alert('um Email Foi Enviado Com Sucesso Para '+ email + ' Verifique Seu Span');
+    window.location.href ='login'
+    
+  }
 }
+}
+
+
