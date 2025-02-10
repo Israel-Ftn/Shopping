846031b5293dfcc14d4e47944a360e2f8a5e0b71
Israel Fiorentini <israelfiorentini189@gmail.com>
2025-02-10 10:57:32 -0300

    aplicando responsividade nas telas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../navbar/navbar.component";
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [NavbarComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
 
+  saldo = 0;
+
+  depositar() {
+    const valor = prompt('Digite um Valor Para Depósito');
+    if (valor !== null) {
+      this.saldo += parseFloat(valor);
+    }
+  }
+  debitar(){
+    const valor=prompt('Digite o valor de Débito:');
+    if(valor !== null){
+      if(this.saldo >= parseFloat(valor)){
+        this.saldo-=parseFloat(valor)
+      }else{
+        alert('saldo Insuficiente')
+      }
+    }
+  }
+  comprarProdutos(preco:number) {
+    if(this.saldo>= preco){
+     this.saldo-= preco;
+     alert('Voce comprou o produto com sucesso  SALDO ATUAL ' + this.saldo)
+    }else{
+      alert('Saldo Insuficiente deposite Dinheiro para Poder Adquirir o Item')
+    }
+  }
 }
